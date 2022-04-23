c102c3fdbf4ab4b1acec07f650b2b256c9c4f0fd
anurag34803 <48412537+anurag34803@users.noreply.github.com>
2022-04-23 19:21:26 +0530

    Update Check_Number_is_Prime_Number_or_not.C

--- Loop Question/Check_Number_is_Prime_Number_or_not.C
+++ Loop Question/Check_Number_is_Prime_Number_or_not.C
@@ -1,27 +1,27 @@
 #include <stdio.h>
-
 void main()
+
 {
     int number;
+    int condition = 0;
     
     printf("Enter Number\n");
     scanf("%d",&number);
     
-    if(number==1)
-    printf("%d is a Prime-Number",number);
-    
-    for(int i=2;i<=number;i++)
+    for(int i=2;i<number;i++)
     {
-        if(number/i==1 && number%i==0)
-        printf("%d is a Prime-Number",number);
-        
-        else if(number/i!=1 && number%i==0)
+        if(number%i==0)
         {
-            printf("%d Number is Not a Prime-Number",number);
+            condition = 1;
             break;
         }
     }
-    
-    
+
+
+if(condition)
+printf("%d is not a Prime-Number",number);
+
+else
+printf("%d is a Prime-Number",number);
 
 }
